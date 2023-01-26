f13de65f6fbf00a7f9c12fa64aa4296f3d82f730
Javid Alakbarov <alakbarovjavid07@gmail.com>
2023-01-26 15:52:30 +0400

    stellTechnology

---
+++ assets/Js/main.js
@@ -0,0 +1,41 @@
+let anchors = document.querySelectorAll('header a[href*="#"]');
+
+for (anchor of anchors) {
+  if (anchor) {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      anchorId = this.getAttribute("href");
+      document.querySelector(anchorId).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
+}
+
+function scroll(){
+	const element = document.getElementById("brands")
+	element.scrollIntoView();
+}
+
+// cards
+const tilt = document.querySelectorAll(".tilt");
+
+VanillaTilt.init(tilt, {
+	reverse: true,
+	max: 15,
+	speed: 400,
+	scale: 1.12,
+	glare: true,
+	reset: true,
+	perspective: 500,
+	transition: true,
+	"max-glare": 0.75,
+	"glare-prerender": false,
+	gyroscope: true,
+	gyroscopeMinAngleX: -45,
+	gyroscopeMaxAngleX: 45,
+	gyroscopeMinAngleY: -45,
+	gyroscopeMaxAngleY: 45
+});
+//end cards
